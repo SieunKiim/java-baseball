5bc2721b5890d8a079ddf7a984e31df6dbfa4976
sieun <sieun960513@naver.com>
2023-09-23 19:07:09 +0900

    feat: 정답 숫자 Collection 생성

---
+++ src/main/java/baseball/AnswerNum.java
@@ -0,0 +1,42 @@
+package baseball;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class AnswerNum {
+    private final List<Integer> answer;
+
+    public AnswerNum(Integer n) {
+        this.answer = Util.toIntegerList(n);
+    }
+    public HashMap<String, Integer> compare(Integer num) {
+        HashMap<String, Integer> result = initCompareResult();
+        List<Integer> integerList = Util.toIntegerList(num);
+        countUpStrikeAndBall(result, integerList);
+        return result;
+    }
+
+    private HashMap<String, Integer> initCompareResult(){
+        HashMap<String, Integer> result = new HashMap<>();
+        result.put("s", 0);
+        result.put("b", 0);
+        return result;
+    }
+
+    private void countUpStrikeAndBall(HashMap<String, Integer> map, List<Integer> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            String temp = getStrikeAndBall(i, arr.get(i));
+            map.put(temp, map.getOrDefault(temp, 0)+ 1);
+        }
+    }
+
+    private String getStrikeAndBall(Integer index, Integer n) {
+        if (!answer.contains(n)) {
+            return null;
+        }
+        if (!answer.get(index).equals(n)) {
+            return "b";
+        }
+        return "s";
+    }
+}
