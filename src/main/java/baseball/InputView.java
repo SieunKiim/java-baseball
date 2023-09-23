fd00bf7544d871609f8ffbe85a445041c4eda485
sieun <sieun960513@naver.com>
2023-09-23 17:28:51 +0900

    chore: apply google convention

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -3,20 +3,21 @@ package baseball;
 import java.util.Scanner;
 
 public class InputView {
+
     Scanner sc = new Scanner(System.in);
 
-    public Integer getNumber(){
+    public Integer getNumber() {
         System.out.print("숫자를 입력해 주세요 : ");
         return Util.parseToInteger(read());
     }
 
-    public Integer getStartOrEnd(){
+    public Integer getStartOrEnd() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Util.parseToInteger(read());
     }
 
-    public String read(){
+    public String read() {
         return sc.nextLine();
     }
 }
