b8c6daf29a3b098234ec492ee581b25d890f0d39
sieun <sieun960513@naver.com>
2023-09-23 18:07:21 +0900

    refactor: 입력, 출력 역할 조정

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -7,17 +7,10 @@ public class InputView {
     Scanner sc = new Scanner(System.in);
 
     public Integer getNumber() {
-        System.out.print("숫자를 입력해 주세요 : ");
         return Util.parseToInteger(read());
     }
 
-    public Integer getStartOrEnd() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        return Util.parseToInteger(read());
-    }
-
-    public String read() {
+    private String read() {
         return sc.nextLine();
     }
 }
--- src/main/java/baseball/ResultView.java
+++ src/main/java/baseball/ResultView.java
@@ -2,6 +2,16 @@ package baseball;
 
 public class ResultView {
 
+    public void inputMessage() {
+        System.out.print("숫자를 입력해 주세요 : ");
+    }
+
+    public void endMessage(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+
     public void outResult(Integer s, Integer b) {
         if (s + b == 0) {
             System.out.println("낫싱");
